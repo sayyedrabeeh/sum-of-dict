356e4eda92c9c10ff3cf7e8829468cefb5d85976
sayyedrabeeh <sayyedrabeeh240@gmail.com>
2024-08-16 10:06:06 +0530

    Add files via upload

---
+++ sum of dict.py
@@ -0,0 +1,16 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+myDict = {'ravi': 10, 'rajnish': 9,
+        'sanjeev': 15, 'yash': 2, 'suraj': 32}
+        
+sum=0
+ 
+for i in myDict.values():
+    sum=sum+i
+print(sum)
